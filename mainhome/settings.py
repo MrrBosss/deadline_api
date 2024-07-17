fdaa3aa63784de10f413d3c7b96ea9075815a889
abbauzb <productgsm@gmail.com>
2024-07-17 15:52:29 +0500

    fix migrations and add settings for production

--- mainhome/settings.py
+++ mainhome/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import datetime
+import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -17,7 +18,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+# See https://env = environ.Env()
+env = environ.Env()
+env.read_env(str(BASE_DIR / ".env"))
+# docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-1@*$sm-=%c_xstt)d)=7!ebxfunl&k(a00)sw+54vv+gtut981'
