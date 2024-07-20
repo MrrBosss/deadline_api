e38b33947df56b7a9b49c3f6288c1d1886f8d394
Boss <abdumuratovabduvali@07gmail.com>
2024-07-20 16:53:06 +0500

    fixed'

--- deadline/urls.py
+++ deadline/urls.py
@@ -3,8 +3,8 @@ from .views import ProjectListView, ProjectDetailView, ProjectCreateView, Projec
 
 urlpatterns = [
     path('', ProjectListView.as_view(), name='project_list'),
-    path('project/<int:pk>/', ProjectDetailView.as_view(), name='project_detail'),
-    path('project/new/', ProjectCreateView.as_view(), name='project_create'),
-    path('project/<int:pk>/edit/', ProjectUpdateView.as_view(), name='project_update'),
-    path('project/<int:pk>/delete/', ProjectDestroyView.as_view(), name='project_delete'),
+    path('<int:pk>/', ProjectDetailView.as_view(), name='project_detail'),
+    path('new/', ProjectCreateView.as_view(), name='project_create'),
+    path('<int:pk>/edit/', ProjectUpdateView.as_view(), name='project_update'),
+    path('<int:pk>/delete/', ProjectDestroyView.as_view(), name='project_delete'),
 ]
